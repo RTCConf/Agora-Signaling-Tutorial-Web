45b9d0fb8d6121c2f0eae96fd67ecf81f1e82030
张乾泽 <qianze.zhang@hotmail.com>
2017-12-18 01:51:53 +0800

    initial version done

--- src/assets/js/meeting.js
+++ src/assets/js/meeting.js
@@ -1,201 +1,259 @@
-(function($) {
+(function ($) {
     $(() => {
-        class Client{
+        class Client {
             //construct a meeting client with signal client and rtc client
-            constructor(sclient, rclient, localAccount){
+            constructor(sclient, localAccount) {
                 this.signal = sclient;
-                this.rtc = rclient;
                 this.localAccount = localAccount;
-
-                //ring tones resources
-                this.sound_ring = new Howl({
-                    src: ['./assets/sound/basic_ring.mp3'],
-                    loop: true
-                });
-
-                this.sound_tones = new Howl({
-                    src: ['./assets/sound/basic_tones.mp3'],
-                    loop: true
-                });
-
-                this.signal.onInviteReceived = $.proxy(this.onInviteReceived, this);
-                this.signal.onInviteEndByPeer = $.proxy(this.onInviteEndByPeer, this);
+                this.current_conversation = null;
+                this.current_msgs = null;
+                // this.cleanData();
+                this.loadFromLocalStorage();
+                this.updateChatList();
 
                 this.subscribeEvents();
             }
 
+            cleanData() {
+                localStorage.setItem("chats", "");
+                localStorage.setItem("messages", "");
+            }
+
+            updateLocalStorage() {
+                localStorage.setItem("chats", JSON.stringify(this.chats));
+                localStorage.setItem("messages", JSON.stringify(this.messages));
+            }
+
+            loadFromLocalStorage() {
+                this.chats = JSON.parse(localStorage.getItem("chats") || "[]");
+                this.messages = JSON.parse(localStorage.getItem("messages") || "{}");
+            }
+
+            updateChatList() {
+                let client = this;
+                let chatsContainer = $(".chat-history");
+                chatsContainer.html("");
+                let html = "";
+                for (var i = 0; i < this.chats.length; i++) {
+                    html += "<li name=\"" + this.chats[i].id + "\" type=\" + this.chats[i].type +\">";
+                    html += "<div class=\"title\">" + this.chats[i].account + "</div>";
+                    html += "<div class=\"desc\">" + this.chats[i].type + "</div>";
+                    html += "</li>";
+                }
+                chatsContainer.html(html);
+
+                $(".chat-history li").off("click").on("click", function () {
+                    let mid = $(this).attr("name");
+                    let type = $(this).attr("type");
+                    if(type === "channel"){
+                        // client.signal.join()
+                        client.showMessage(mid);
+                    } else {
+                        client.showMessage(mid);
+                    }
+                });
+
+                if (this.chats.length > 0) {
+                    this.showMessage(this.chats[0].id);
+                }
+            }
+
+            showMessage(mid) {
+                let client = this;
+                this.current_msgs = this.messages[mid] || [];
+                let conversation = this.chats.filter(function (item) {
+                    return (item.id + "") === (mid + "");
+                })
+                if (conversation.length === 0) {
+                    return;
+                }
+                this.current_conversation = conversation[0];
+                this.current_msgs = this.messages[this.current_conversation.id] || [];
+
+
+                $('#message-to-send').off("keydown").on("keydown", function (e) {
+                    if (e.keyCode == 13) {
+                        e.preventDefault();
+                        client.sendMessage($(this).val());
+                        $(this).val("");
+                    }
+                });
+
+                $(".chat-messages").html("");
+                let html = "";
+                for (let i = 0; i < this.current_msgs.length; i++) {
+                    html += this.buildMsg(this.current_msgs[i].text, this.current_msgs[i].account === this.localAccount, this.current_msgs[i].ts);
+                }
+                $(".chat-history li").removeClass("selected");
+                $(".chat-history li[name=" + mid + "]").addClass("selected");
+                $(".chat-messages").html(html);
+                $(".detail .nav").html(conversation[0].account);
+            }
+
+            sendMessage(text) {
+                if (!this.current_msgs) {
+                    return;
+                }
+                let msg_item = { ts: new Date(), text: text, account: this.localAccount };
+                this.current_msgs.push(msg_item);
+                if (this.current_conversation.type === "instant") {
+                    this.signal.sendMessage(this.current_conversation.account, text);
+                } else {
+                    this.signal.messageChannelSend(text);
+                }
+
+                $(".chat-messages").append(this.buildMsg(text, true, msg_item.ts));
+
+                this.updateMessageMap();
+            }
+
+            updateMessageMap(c, m) {
+                let conversation = c || this.current_conversation;
+                let msgs = m || this.current_msgs;
+                this.messages[conversation.id] = msgs;
+                this.updateLocalStorage();
+            }
+
             //return a promise resolves a remote account name
-            requestRemoteAccount(){
+            addConversation() {
                 let deferred = $.Deferred();
-                let dialog = $(".remoteAccountModal");
+                let dialog = $(".conversation-modal");
                 let accountField = dialog.find(".remoteAccountField");
                 let localAccount = this.localAccount;
-                
-                dialog.find(".callBtn").off("click").on("click", (e) => {
+                let client = this;
+
+                dialog.find(".confirmBtn").off("click").on("click", (e) => {
                     //dialog confirm
-                    let account = $(".remoteAccountField").val();
+                    let account = $(".conversation-target-field").val();
+                    let type = $(':radio[name="type"]').filter(':checked').val();
+                    let conversations = client.chats.filter(function(item){
+                        return item.account === account;
+                    });
 
                     if (!account) {
-                        $(".remoteAccountField").siblings(".invalid-feedback").html("Valid account should be a non-empty numeric value.")
-                        $(".remoteAccountField").removeClass("is-invalid").addClass("is-invalid");
-                    } else if(`${account}` === `${localAccount}`) {
-                        $(".remoteAccountField").siblings(".invalid-feedback").html("You can't call yourself.")
-                        $(".remoteAccountField").removeClass("is-invalid").addClass("is-invalid");
+                        $(".conversation-target-field").siblings(".invalid-feedback").html("请输入一个合法的名字.")
+                        $(".conversation-target-field").removeClass("is-invalid").addClass("is-invalid");
+                    } else if (`${account}` === `${localAccount}`) {
+                        $(".conversation-target-field").siblings(".invalid-feedback").html("你不能跟自己聊天.")
+                        $(".conversation-target-field").removeClass("is-invalid").addClass("is-invalid");
+                    } else if (conversations.length > 0){
+                        $(".conversation-target-field").siblings(".invalid-feedback").html("该聊天已存在.")
+                        $(".conversation-target-field").removeClass("is-invalid").addClass("is-invalid");
                     } else {
-                        $(".startCallBtn").hide();
                         dialog.modal('hide');
+                        client.chats.splice(0, 0, { id: new Date().getTime(), account: account, type: type });
+                        client.updateLocalStorage();
+                        client.updateChatList();
                         deferred.resolve(account);
                     }
                 });
 
+                dialog.find(".cancelBtn").off("click").on("click", (e) => {
+                    //dialog confirm
+                    dialog.modal('hide');
+                    deferred.reject();
+                });
+
                 //start modal
-                dialog.modal({backdrop: "static", focus: true});
+                dialog.modal({ backdrop: "static", focus: true });
 
                 return deferred;
             }
 
-            //return a promise resolves a signaling call result
-            call(channelName, account, requirePeerOnline){
-                let deferred = $.Deferred();
-                let dialog = $(".callingModal");
-                dialog.find(".callee").html(account);
-                let signal = this.signal;
-
-                signal.call(channelName, account, requirePeerOnline).done(_ => {
-                    dialog.modal('hide');
-                    deferred.resolve();
-                }).catch($.proxy(err => {
-                    Message.show(err.reason);
-                    deferred.reject();
-                }, this));
-
-                return deferred.promise();
-            }
-
-            //end given call object, passive means the call is ended by peer
-            endCall(call, passive){
-                let deferred = $.Deferred();
-                let signal = this.signal;
-                let rtc = this.rtc;
-                let btn = $(".toolbar .muteBtn");
-
-                $(".startCallBtn").show();
-
-                rtc.muted = true;
-                btn.removeClass("btn-info").addClass("btn-secondary");
-                btn.find("i").html("mic");
-
-                //end rtc
-                rtc.end();
-                //end signal call
-                signal.endCall(call, passive);
-                return deferred.promise();
-            }
-
-            //ring when calling someone else
-            ringCalling(play){
-                if(play){
-                    this.sound_ring.play();
-                } else {
-                    this.sound_ring.stop();
-                }
-            }
-            //ring when being called by someone else
-            ringCalled(play){
-                if(play){
-                    this.sound_tones.play();
-                } else {
-                    this.sound_tones.stop();
-                }
-            }
-
             //events
-            subscribeEvents(){
+            subscribeEvents() {
                 let signal = this.signal;
-                //toolbar end call btn
-                $(".toolbar .endCallBtn").off("click").on("click", $.proxy(e => {
-                    this.ringCalling(false);
-                    this.endCall(signal.call_active || signal.call_holding, false);
-                }, this));
+                let client = this;
 
-                //toolbar mute btn
-                $(".toolbar .muteBtn").off("click").on("click", $.proxy(e => {
-                    let btn = $(e.currentTarget);
-                    let rtc = this.rtc;
-                    rtc.toggleMute();
-                    if(rtc.muted){
-                        btn.removeClass("btn-secondary").addClass("btn-info");
-                        btn.find("i").html("mic_off");
-                    } else {
-                        btn.removeClass("btn-info").addClass("btn-secondary");
-                        btn.find("i").html("mic");
+                $(".new-conversation-btn").off("click").on("click", function () {
+                    client.addConversation();
+                });
+
+                $(':radio[name="type"]').change(function () {
+                    var type = $(this).filter(':checked').val();
+                    var field = $(".conversation-target-field");
+                    switch (type) {
+                        case "instant":
+                            field.attr("placeholder", "输入聊天对象账号");
+                            break;
+                        case "channel":
+                            field.attr("placeholder", "输入聊天频道");
+                            break;
                     }
-                }, this));
-
-                $(".startCallBtn").off("click").on("click", $.proxy(e => {
-                    this.requestRemoteAccount().done($.proxy(remoteAccount => {
-                        //start calling via signal
-                        if(remoteAccount !== ""){
-                            this.ringCalling(true);
-                            $.when(rtc.init(channelName, false), this.call(channelName, remoteAccount, true)).done($.proxy((stream, _) => {
-                                this.ringCalling(false);
-                                this.rtc.rtc.publish(stream);
-                            }, this)).catch($.proxy(_ => {
-                                this.ringCalling(false);
-                                this.endCall(signal.call_active || signal.call_holding, false);
-                            }, this));
-                        }
-                    }, this));
-                }, this));
-            }
-
-            //delegate callback when receiving call
-            onInviteReceived(call){
-                let dialog = $(".calledModal");
-                let signal = this.signal;
-                let rtc = this.rtc;
-
-                dialog.find(".caller").html(call.peer);
-                dialog.find(".declineBtn").off("click").on("click", (e) => {
-                    dialog.modal('hide');
-                    this.ringCalled(false);
-                    signal.rejectCall(call, 0);
                 });
 
-                dialog.find(".acceptBtn").off("click").on("click", (e) => {
-                    dialog.modal('hide');
-                    $(".startCallBtn").hide();
-                    this.ringCalled(false);
-                    signal.acceptCall(call).done(call => {
-                        rtc.init(call.channelName, true);
-                    }).catch(err => {
-                        Logger.log(`Accept call failed: ${err}`);
-                    });
-                });
-
-                this.ringCalled(true);
-                dialog.modal({backdrop: "static"});
+                signal.onMessageInstantReceive = $.proxy(this.onReceiveMessage, this);
             }
 
-            //delegate callback called when call end by peer
-            onInviteEndByPeer(){
-                let signal = this.signal;
-                $(".calledModal").modal('hide');
-                this.ringCalled(false);
-                this.endCall(signal.call_active || signal.call_holding ,true);
+            onReceiveMessage(account, msg, type) {
+                let client = this;
+                var conversations = this.chats.filter(function (item) {
+                    return item.account === account;
+                });
+
+                if (conversations.length === 0) {
+                    //no conversation yet, create one
+                    conversations = [{ id: new Date().getTime(), account: account, type: type }];
+                    client.chats.splice(0, 0, conversations[0]);
+                    client.updateLocalStorage();
+                    client.updateChatList();
+                }
+
+                for (let i = 0; i < conversations.length; i++) {
+                    let conversation = conversations[i];
+
+                    let msgs = this.messages[conversation.id] || [];
+                    let msg_item = { ts: new Date(), text: msg, account: account };
+                    msgs.push(msg_item);
+                    this.updateMessageMap(conversation, msgs);
+
+                    if (conversation.id+"" === this.current_conversation.id+"") {
+                        $(".chat-messages").append(client.buildMsg(msg, false, msg_item.ts));
+                    }
+                }
+            }
+
+            buildMsg(msg, me, ts){
+                let html = "";
+                let className = me ? "message right" : "message";
+                html += "<li class=\"" + className +"\">";
+                html += "<img src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/1_copy.jpg\">";
+                html += "<div class=\"bubble\">" + msg + "<div class=\"corner\"></div>";
+                html += "<span>" + this.parseTwitterDate(ts) + "</span></div></li>";
+
+                return html;
+            }
+
+            parseTwitterDate(tdate) {
+                var system_date = new Date(Date.parse(tdate));
+                var user_date = new Date();
+                // if (K.ie) {
+                //     system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
+                // }
+                var diff = Math.floor((user_date - system_date) / 1000);
+                if (diff <= 1) {return "just now";}
+                if (diff < 20) {return diff + " seconds ago";}
+                if (diff < 40) {return "half a minute ago";}
+                if (diff < 60) {return "less than a minute ago";}
+                if (diff <= 90) {return "one minute ago";}
+                if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+                if (diff <= 5400) {return "1 hour ago";}
+                if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+                if (diff <= 129600) {return "1 day ago";}
+                if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+                if (diff <= 777600) {return "1 week ago";}
+                return "on " + system_date;
             }
         }
 
         const appid = "672fac5cd7194d26908a15900c6d6484", appcert = "a31c1044fe4040ba8d1af4ba3f5165f9";
         let localAccount = Browser.getParameterByName("account");
         let signal = new SignalingClient(appid, appcert);
-        let rtc = new RtcClient(appid, appcert);
-        let client = new Client(signal, rtc, localAccount);
+        let client = new Client(signal, localAccount);
         let channelName = Math.random() * 10000 + "";
         //by default call btn is disabled
         signal.login(localAccount).done(_ => {
             //once logged in, enable the call btn
-            $(".startCallBtn").prop("disabled", false);
         });
     });
 }(jQuery));
--- src/assets/js/signalingClient.js
+++ src/assets/js/signalingClient.js
@@ -1,14 +1,12 @@
 class SignalingClient{
     constructor(appid, appcertificate){
         this.signal = Signal(appid);
-        this.call_holding = null;
-        this.call_active = null;
         this.channel = null;
         this.appid = appid;
         this.appcert = appcertificate;
         this.uid = null;
 
-        this.onInviteReceived = null;
+        this.onMessageInstantReceive = null;
         this.onInviteEndByPeer = null;
     }
 
@@ -34,51 +32,14 @@ class SignalingClient{
             deferred.reject();
         }, this);
 
-        session.onInviteReceived = $.proxy(this._onInviteReceived, this);
+        session.onMessageInstantReceive = $.proxy(this._onMessageInstantReceive, this);
         this.session = session;
 
         return deferred.promise();
     }
 
-    call(channelName, peer, require_peer_online){
-        let deferred = $.Deferred();
-        let extra = {};
-
-        if(require_peer_online){
-            extra["_require_peer_online"] = 1;
-        }
-
-        let extra_msg = JSON.stringify(extra);
-
-        Logger.log('call ' + peer + ' , channelName : ' + channelName + ', extra : ' + extra_msg);
-
-        let call = this.session.channelInviteUser2 ( channelName, peer, extra_msg);
-
-        call.onInviteAcceptedByPeer = $.proxy(extra => {
-            this.call_active = this.call_holding;
-            this.call_holding = null;
-            this.join(call.channelName).then(_ => {
-                Logger.log('call.onInviteAcceptedByPeer ' + extra);
-                deferred.resolve();
-            });
-        }, this);
-
-        call.onInviteRefusedByPeer = $.proxy(extra => {
-            Logger.log(`call.onInviteRefusedByPeer ${extra}`);
-            let status = JSON.parse(extra).status;
-            deferred.reject({reason: `Call refused. ${this.statusText(status)}`});
-        }, this);
-        
-        call.onInviteFailed = $.proxy(extra => {
-            Logger.log(`call.onInviteFailed ${extra}`);
-            deferred.reject({reason: `Invite failed: ${JSON.parse(extra).reason}`});
-        }, this);
-
-        call.onInviteEndByPeer = $.proxy(this._onInviteEndByPeer, this);
-
-        this.call_holding = call;
-
-        return deferred.promise();
+    sendMessage(account, text){
+        this.session.messageInstantSend(account, text);
     }
 
     join(channelName){
@@ -118,82 +79,9 @@ class SignalingClient{
         return deferred;
     }
 
-    acceptCall(call){
-        let deferred = $.Deferred();
-        this.call_active = this.call_holding;
-        this.call_holding = null;
-
-        this.join(call.channelName).done(_ => {
-            call.channelInviteAccept();
-            deferred.resolve({
-                peer: call.peer,
-                channelName: call.channelName
-            });
-        }).catch(err => {
-            deferred.reject(err);
-        });
-
-        return deferred.promise();
-    }
-
-    rejectCall(call, status){
-        let deferred = $.Deferred();
-        status = status || 0;
-        call.channelInviteRefuse(JSON.stringify({status: status}));
-        return deferred.resolve().promise();
-    }
-
-    endCall(call, passive){
-        let deferred = $.Deferred();
-        let channel = this.channel;
-        
-        call.onInviteEndByMyself = $.proxy(extra => {
-            Logger.log('call.onInviteEndByMyself ' + extra);
-            this.call_holding = (this.call_holding === call) ? null : this.call_holding;
-            this.call_active = (this.call_active === call) ? null : this.call_active;
-            this.leave();
-        }, this);
-
-        if(!passive){
-            call.channelInviteEnd();
-        } else {
-            this.call_active = null;
-            this.call_holding = null;
-        }
-        return deferred.promise();
-    }
-
-    statusText(status){
-        switch(status){
-            case 0:
-            return "Peer rejected.";
-            case 1:
-            return "Peer is busy.";
-        }
-    }
-
-    //session events delegate
-    _onInviteReceived(call){
-        Logger.log(`recv invite from ${call.peer}, ${call.channelName}, ${call.extra}`);
-
-        //incoming call for accept or refuse
-        if(this.call_active !== null){
-            //busy
-            this.rejectCall(call, 1);
-        } else {
-            call.onInviteEndByPeer = $.proxy(this._onInviteEndByPeer, this);
-            this.call_holding = call;
-            if(this.onInviteReceived !== null){
-                this.onInviteReceived(call);
-            }
-        }
-    }
-
-    //call events delegate
-    _onInviteEndByPeer(extra){
-        Logger.log('call.onInviteEndByPeer ' + extra);
-        if(this.onInviteEndByPeer !== null){
-            this.onInviteEndByPeer();
+    _onMessageInstantReceive(account, uid, msg){
+        if(this.onMessageInstantReceive){
+            this.onMessageInstantReceive(account, msg, "instant");
         }
     }
 }
